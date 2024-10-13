9a79946f2939dd207350624639885fc0385d6f4a
LordYoni <yoni.taine@etud.u-picardie.fr>
2024-10-13 11:00:14 +0200

    Add INSERT privilege for 'dbaccess' user in MariaDB configuration

--- Raspberry/run.py
+++ Raspberry/run.py
@@ -1,8 +1,19 @@
 import serial
+import mysql.connector as MC
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def add_comma(arr):
+    if arr < 10:
+        return str(".0" + str(arr))
+    else:
+        return str("." + str(arr))
 
 # Configuration of serial communication parameters
 timeout_sec = 3
-input_size = 13
+input_size = 16
 
 # Definition of start and end bytes
 start_byte = 0xa0
@@ -14,6 +25,30 @@ serial_in = serial.Serial('/dev/ttyUSB0', 9600)  # ,serial.EIGHTBITS,serial.PARI
 # Resetting the output buffer (necessary?)
 serial_in.reset_output_buffer()
 
+def insert_data(tds, ph, oxygen, conductivity, temperature):
+    query = """
+        INSERT INTO Data (TDS, pH, Oxygen, Conductivity, Temperature)
+        VALUES (%s, %s, %s, %s, %s)
+    """
+    args = (tds, ph, oxygen, conductivity, temperature)
+
+    try:
+        conn = MC.connect(
+            host=os.getenv('DB_HOST'),
+            database=os.getenv('DB_NAME'),
+            user=os.getenv('DB_USER'),
+            password=os.getenv('DB_PASSWORD')
+        )
+        cursor = conn.cursor()
+        cursor.execute(query, args)
+        conn.commit()
+        print("Data inserted successfully")
+    except MC.Error as err:
+        print("Error while inserting data:", err)
+    finally:
+        cursor.close()
+        conn.close()
+        
 while True:
     # Resetting the input buffer
     serial_in.reset_input_buffer()
@@ -23,11 +58,13 @@ while True:
 
     # Checking the size of the read data
     if len(array_in) == input_size:
+
         # Checking the start and end bytes
         if array_in[0] == start_byte and array_in[-1] == end_byte:
-            clc_checksum = 0
 
             # Calculating the checksum
+            clc_checksum = 0
+            
             for i in range(1, input_size - 2):
                 clc_checksum += array_in[i]
 
@@ -36,22 +73,30 @@ while True:
             # Verifying the checksum
             if clc_checksum == array_in[-2]:
                 # Converting data to usable values
-                temperature = float(str(int.from_bytes([array_in[1]], byteorder='big', signed=True)) + "." + str(array_in[2]))
-                ph = float(str(array_in[3]) + "." + str(array_in[4]))
+                temperature = float(str(int.from_bytes([array_in[1]], byteorder='big', signed=True)) + add_comma(array_in[2]))
+                ph = float(str(array_in[3]) + add_comma(array_in[4]))
 
                 temp = str(array_in[5] * 256 + array_in[6])
-                tds = float(temp + "." + str(array_in[7]))
+                tds = float(temp + add_comma(array_in[7]))
 
                 temp = str(array_in[8] * 256 + array_in[9])
-                ec = float(temp + "." + str(array_in[10]))
+                ec = float(temp + add_comma(array_in[10]))
+
+                temp = str(array_in[11] * 256 + array_in[12])
+                ox = float(temp + add_comma(array_in[13]))
+
+                for i in range(1, input_size - 1):
+                    print(hex(array_in[i]))
 
                 print("Temperature: " + str(temperature))
                 print("pH: " + str(ph))
                 print("TDS: " + str(tds))
                 print("EC: " + str(ec))
-                print("Data received successfully")
+                print("OX: " + str(ox))
 
-                # TODO: send data to database
+                print("Data received successfully\n")
+
+                insert_data(tds, ph, ox, ec, temperature)
 
             else:
                 print("Checksum error")
